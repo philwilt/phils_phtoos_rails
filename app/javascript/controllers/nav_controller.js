ed5509ba828ab49c95bd2f9b856b5fdead94ee73
Phil Wilt <phillip.wilt@gmail.com>
2025-07-17 17:02:26 -0700

    Nav toggle

--- app/javascript/controllers/nav_controller.js
+++ app/javascript/controllers/nav_controller.js
@@ -2,15 +2,25 @@ import { Controller } from "@hotwired/stimulus"
 
 
 export default class extends Controller {
-  static targets = ["button"]
+  static targets = ["button", "pane"]
 
   initialize() {
     this.open = false;
+    this.paneTarget.style.display = "none"
   }
 
   toggle() {
     this.open = !this.open;
-    this.buttonTarget.classList.toggle("fa-bars")
-    this.buttonTarget.classList.toggle("fa-chevron-right")
+    this.toggleIcon();
+    this.togglePane();
+  }
+
+  toggleIcon() {
+    this.buttonTarget.classList.toggle("fa-bars");
+    this.buttonTarget.classList.toggle("fa-chevron-right");
+  }
+
+  togglePane() {
+    this.paneTarget.style.display = this.open  ? '' : 'none'
   }
 }
